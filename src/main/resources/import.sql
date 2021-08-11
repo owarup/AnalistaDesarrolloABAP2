67001d81de3250c81c099c6900a32a024dbcd13a
omar elizarraraz <32749731+owarup@users.noreply.github.com>
2021-08-11 16:11:51 -0500

    imports.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,36 @@
+CREATE DATABASE `springbootdb` /*!40100 COLLATE 'utf8_general_ci' */
+
+CREATE TABLE `usuario`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`nombre_usuario` VARCHAR(50) NOT NULL DEFAULT '',
+	`contrasena` VARCHAR(50) NOT NULL DEFAULT '',
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `nombre_usuario` (`nombre_usuario`)
+)
+COLLATE='latin1_swedish_ci'
+;
+
+SHOW CREATE TABLE `springbootdb`.`usuario`;
+
+INSERT INTO usuario (nombre_usuario,contrasena) VALUES ('admin','admin');
+
+CREATE TABLE `productos` (
+	`id` INT(15) NOT NULL AUTO_INCREMENT,
+	`color` VARCHAR(10) NOT NULL DEFAULT 'blanco' COLLATE 'latin1_swedish_ci',
+	`dimension` VARCHAR(10) NULL COLLATE 'latin1_swedish_ci',
+	`capacidad` VARCHAR(10) NULL COLLATE 'latin1_swedish_ci',
+	`modelo` VARCHAR(20) NOT NULL DEFAULT 'unico' COLLATE 'latin1_swedish_ci',
+	`material` VARCHAR(20) NOT NULL DEFAULT 'no especificada' COLLATE 'latin1_swedish_ci',
+	`calidad` VARCHAR(10) NOT NULL DEFAULT 'buena' COLLATE 'latin1_swedish_ci',
+	`cantidad` INT(10) NOT NULL,
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
+
+
+SHOW CREATE TABLE `springbootdb`.`productos`;
 
 
 insert into productos (color,dimension,capacidad,modelo,material,calidad,cantidad) values ('roja','150x145','125', 'trinidad', 'ceramica', 'buena','5');
